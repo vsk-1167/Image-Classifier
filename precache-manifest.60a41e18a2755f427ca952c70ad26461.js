730238783a991c37028ebf6e9902a0d3ecd1a140
Vrishab Sathish Kumar <skvdev17@uw.edu>
2020-09-15 02:45:00 -0700

    Updates

---
+++ precache-manifest.60a41e18a2755f427ca952c70ad26461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9b687237747b15afd04c82e8bd99a0",
+    "url": "/image-classifier/index.html"
+  },
+  {
+    "revision": "072b4c25db5c27866318",
+    "url": "/image-classifier/static/css/main.1d1af5da.chunk.css"
+  },
+  {
+    "revision": "2bbb9d24eb87383132ae",
+    "url": "/image-classifier/static/js/2.521f4db0.chunk.js"
+  },
+  {
+    "revision": "7d1906d202e927039094444d5b221699",
+    "url": "/image-classifier/static/js/2.521f4db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "072b4c25db5c27866318",
+    "url": "/image-classifier/static/js/main.e726f60a.chunk.js"
+  },
+  {
+    "revision": "5617a32df545943cb2cf",
+    "url": "/image-classifier/static/js/runtime-main.ddbe4612.js"
+  }
+]);
